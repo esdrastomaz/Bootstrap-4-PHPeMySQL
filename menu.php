85ff02794cede7b9ba7faa4daf0ef850c288707b
Esdras Tomaz <esdras_tomaz2@hotmail.com>
2021-01-29 02:54:32 -0300

    inicio modulo financeiro

--- menu.php
+++ menu.php
@@ -16,14 +16,24 @@ require_once "dependencias.php";
                         Cadastros
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="listarProdutos.php"><i class="fas fa-shopping-cart"></i></i>&nbsp;Cadastro de Produtos</a>
+                        <a class="dropdown-item" href="listarProdutos.php"><i class="fas fa-shopping-cart"></i>&nbsp;Cadastro de Produtos</a>
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="listarClientes.php"><i class="fas fa-users"></i>&nbsp;Cadastro de Clientes</a>                        
                         <div class="dropdown-divider"></div>
                         <a class="dropdown-item" href="#"><i class="fas fa-address-book"></i>&nbsp;Cadastro de Fornecedores</a>
                         <div class="dropdown-divider"></div>
-                        <a class="dropdown-item" href="#"><i class="fas fa-bars"></i>&nbsp;Cadastro de Categorias</a>
-                        
+                        <a class="dropdown-item" href="#"><i class="fas fa-bars"></i>&nbsp;Cadastro de Categorias</a>                        
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Financeiro
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="#"><i class="fas fa-file-invoice-dollar"></i>&nbsp;Contas a Pagar</a>
+                        <div class="dropdown-divider"></div>
+                        <a class="dropdown-item" href="#"><i class="fas fa-hand-holding-usd"></i>&nbsp;Contas a Receber</a>                       
+                      
                     </div>
                 </li>
             </ul>
